31e3ec1ab67659d782ef3d7b8242eef60c5080a2
Rumesh Hapuarachchi <rehrumesh@hotmail.com>
2016-07-15 15:23:27 +0530

    Modified execute code

--- dynamodb/transformations.js
+++ dynamodb/transformations.js
@@ -16,11 +16,13 @@ var createTable = function (dynamodb, migration) {
         });
     });
 };
+*/
 
-var formatTableName = function (tranformation, options) {
+var formatTableName = function(transformation, options) {
     return options.tablePrefix + transformation.Table.TableName + options.tableSuffix;
 };
 
+/*
 var runSeeds = function (dynamodb, migration) {
     var params,
         batchSeeds = migration.Seeds.map(function (seed) {
@@ -57,8 +59,8 @@ var runSeeds = function (dynamodb, migration) {
 };
 */
 
-var create = function (transformationName, options) {
-    return new BbPromise(function (resolve, reject) {
+var create = function(transformationName, options) {
+    return new BbPromise(function(resolve, reject) {
         var template = require('./templates/template');
         template.Table.TableName = transformationName;
 
@@ -66,7 +68,7 @@ var create = function (transformationName, options) {
             fs.mkdirSync(options.dir);
         }
 
-        fs.writeFile(options.dir + '/' + transformationName + '.js', template, function (err) {
+        fs.writeFile(options.dir + '/' + transformationName + '.js', template, function(err) {
             if (err) {
                 return reject(err);
             } else {
@@ -77,9 +79,9 @@ var create = function (transformationName, options) {
 };
 module.exports.create = create;
 
-var executeAll = function (dynamodb, options) {
-    return new BbPromise(function (resolve, reject) {
-        fs.readdirSync(options.dir).forEach(function (file) {
+var executeAll = function(dynamodb, options) {
+    return new BbPromise(function(resolve, reject) {
+        fs.readdirSync(options.dir).forEach(function(file) {
             var transformation = require(options.dir + '/' + file);
             transformation.Table.TableName = formatTableName(transformation, options);
             /* 1) Scan sets table records asynchronously
@@ -91,14 +93,76 @@ var executeAll = function (dynamodb, options) {
 };
 module.exports.executeAll = executeAll;
 
-var execute = function (dynamodb, options) {
-    return new BbPromise(function (resolve, reject) {
+var execute = function(dynamodb, options) {
+    return new BbPromise(function(resolve, reject) {
         var transformation = require(options.dir + '/' + options.transformationName + '.js');
-        transformation.Table.TableName = formatTableName(transformation, options);
-        /* 1) Scan sets table records asynchronously
-           2) Call transformation.Transform method for the set
-           3) Batch Update these records
-        */
+        transformation.TableName = formatTableName(transformation, options);
+
+        var lastKey = "",
+            isExecuting = false,
+            readParams = {
+                TableName: transformation.TableName,
+                Limit: 25,
+                Select: 'ALL_ATTRIBUTES'
+            },
+            processWriteDataset = function(items, outArray) {
+                items.forEach(function(item) {
+                    var tmpItem = {
+                        'PutRequest': {
+                            'Item': item
+                        }
+                    };
+                    outArray.push(tmpItem);
+                })
+            },
+            executeReadWriteCycle = function(rparams) {
+                isExecuting = true;
+                dynamodb.doc.scan(rparams, function(err, data) {
+                    if (err) {
+                        reject("Scan failed");
+                    } else {
+                        lastKey = data.LastEvaluatedKey ? data.LastEvaluatedKey : "";                        
+
+                        if (data.Items) {
+                            var processedData = [],
+                                writeParams = {
+                                    RequestItems: {}
+                                };
+                            data.Items.forEach(function(item) {
+                                transformation.Transform(item);
+                            })
+                            processWriteDataset(data.Items, processedData);
+                            writeParams[transformation.Table.TableName] = processedData;
+
+                            dynamodb.doc.batchWrite(writeParams, function(err, data) {
+                                if (err) {
+                                    isExecuting = false;
+                                    reject('write failed' + writeParams);
+                                } else {
+                                    isExecuting = false;
+                                }
+                            });
+
+                        }
+                    }
+                });
+            };
+
+        executeReadWriteCycle(readParams);
+
+        var intvl = setInterval(function() {
+            if (!isExecuting) {
+                if (lastKey !== "") {
+                    clearInterval(intvl);
+                    resolve();
+                } else {
+                    readParams["ExclusiveStartKey"] = lastKey;
+                    executeReadWriteCycle(readParams);
+                }
+            }
+        }, 10);
+
+
     });
 };
 module.exports.execute = execute;
