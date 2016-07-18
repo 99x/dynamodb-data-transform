54d74da10e362f195ed3f55b76f969abf744e971
Rumesh Hapuarachchi <rehrumesh@hotmail.com>
2016-07-18 08:35:01 +0530

    Added execute function

--- dynamodb/transformations.js
+++ dynamodb/transformations.js
@@ -3,61 +3,9 @@
 var BbPromise = require('bluebird'),
     fs = require('fs');
 
-/*
-var createTable = function (dynamodb, migration) {
-    return new BbPromise(function (resolve) {
-        dynamodb.raw.createTable(migration.Table, function (err) {
-            if (err) {
-                console.log(err);
-            } else {
-                console.log("Table creation completed for table: " + migration.Table.TableName);
-            }
-            resolve(migration);
-        });
-    });
-};
-*/
-
 var formatTableName = function(transformation, options) {
-    return options.tablePrefix + transformation.Table.TableName + options.tableSuffix;
+    return options.tablePrefix + transformation.TableName + options.tableSuffix;
 };
-
-/*
-var runSeeds = function (dynamodb, migration) {
-    var params,
-        batchSeeds = migration.Seeds.map(function (seed) {
-            return {
-                PutRequest: {
-                    Item: seed
-                }
-            };
-        });
-    params = {
-        RequestItems: {}
-    };
-    params.RequestItems[migration.Table.TableName] = batchSeeds;
-    return new BbPromise(function (resolve, reject) {
-        var interval = 0,
-            execute = function (interval) {
-                setTimeout(function () {
-                    dynamodb.doc.batchWrite(params, function (err) {
-                        if (err) {
-                            if (err.code === "ResourceNotFoundException" && interval <= 5000) {
-                                execute(interval + 1000);
-                            } else {
-                                reject(err);
-                            }
-                        } else {
-                            console.log("Seed running complete for table: " + migration.Table.TableName);
-                            resolve(migration);
-                        }
-                    });
-                }, interval);
-            };
-        execute(interval);
-    });
-};
-*/
 
 var create = function(transformationName, options) {
     return new BbPromise(function(resolve, reject) {
@@ -83,7 +31,7 @@ var executeAll = function(dynamodb, options) {
     return new BbPromise(function(resolve, reject) {
         fs.readdirSync(options.dir).forEach(function(file) {
             var transformation = require(options.dir + '/' + file);
-            transformation.Table.TableName = formatTableName(transformation, options);
+            transformation.TableName = formatTableName(transformation, options);
             /* 1) Scan sets table records asynchronously
                2) Call transformation.Transform method for the set
                3) Batch Update these records
@@ -100,6 +48,7 @@ var execute = function(dynamodb, options) {
 
         var lastKey = "",
             isExecuting = false,
+            updatedCount = 0,
             readParams = {
                 TableName: transformation.TableName,
                 Limit: 25,
@@ -113,33 +62,35 @@ var execute = function(dynamodb, options) {
                         }
                     };
                     outArray.push(tmpItem);
-                })
+                });
             },
             executeReadWriteCycle = function(rparams) {
                 isExecuting = true;
                 dynamodb.doc.scan(rparams, function(err, data) {
                     if (err) {
-                        reject("Scan failed");
+                        reject("Scan failed"+err+ JSON.stringify(rparams));
                     } else {
-                        lastKey = data.LastEvaluatedKey ? data.LastEvaluatedKey : "";                        
-
+                        lastKey = data.LastEvaluatedKey ? data.LastEvaluatedKey : "";
                         if (data.Items) {
                             var processedData = [],
                                 writeParams = {
                                     RequestItems: {}
                                 };
+                                
                             data.Items.forEach(function(item) {
-                                transformation.Transform(item);
-                            })
+                                transformation.Transformation(item);
+                            });
+
                             processWriteDataset(data.Items, processedData);
-                            writeParams[transformation.Table.TableName] = processedData;
+                            writeParams.RequestItems[transformation.TableName] = processedData;
 
                             dynamodb.doc.batchWrite(writeParams, function(err, data) {
                                 if (err) {
                                     isExecuting = false;
-                                    reject('write failed' + writeParams);
+                                    reject('updated : '+updatedCount+' write failed' +err + JSON.stringify(writeParams) );
                                 } else {
                                     isExecuting = false;
+                                    updatedCount += writeParams.RequestItems[transformation.TableName].length;
                                 }
                             });
 
@@ -152,9 +103,9 @@ var execute = function(dynamodb, options) {
 
         var intvl = setInterval(function() {
             if (!isExecuting) {
-                if (lastKey !== "") {
+                if (lastKey === "") {
                     clearInterval(intvl);
-                    resolve();
+                    resolve("Updated "+updatedCount+ " records");
                 } else {
                     readParams["ExclusiveStartKey"] = lastKey;
                     executeReadWriteCycle(readParams);
